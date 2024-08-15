eadd2980d7a572b72d2e25e5ed9259e1f6bf91df
Gorchanyuk <gorchanyk@gmail.com>
2024-08-15 12:27:11 +0400

    Написал тест для consumer.receive

---
+++ src/test/java/ru/gorchanyuk/artemis/tdd/service/ConsumerTest.java
@@ -0,0 +1,30 @@
+package ru.gorchanyuk.artemis.tdd.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.gorchanyuk.artemis.tdd.XmlIn;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Тестирование получения сообщений")
+public class ConsumerTest {
+
+    @Mock
+    private Producer producer;
+    @InjectMocks
+    private Consumer consumer;
+
+    @Test
+    void testGetMessage() {
+        XmlIn in = new XmlIn();
+
+        consumer.receive(in);
+
+        verify(producer).send();
+    }
+}
