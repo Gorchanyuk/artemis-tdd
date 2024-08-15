47604e8d329b0338c3df9edcbcc28cb2594edee8
Gorchanyuk <gorchanyk@gmail.com>
2024-08-15 12:25:49 +0400

    Init

---
+++ src/main/java/ru/gorchanyuk/artemis/tdd/ArtemisTddApplication.java
@@ -0,0 +1,13 @@
+package ru.gorchanyuk.artemis.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtemisTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gorchanyuk/artemis/tdd/ArtemisTddApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gorchanyuk.artemis.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemisTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
